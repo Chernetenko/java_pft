b57c4bc3013d56a7ca5b9866632073dbe342b57f
chernatenko <maria.chernetenko@gmail.com>
2018-10-16 16:50:31 +0300

    Добавление группы

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/GroupCreationTests.java
@@ -19,26 +19,26 @@ public class GroupCreationTests {
     driver = new FirefoxDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
+  }
+
+  @Test
+  public void test900() throws Exception {
+    driver.get("http://localhost/addressbook/group.php");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test8");
+    driver.findElement(By.name("group_name")).sendKeys("test");
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test8");
+    driver.findElement(By.name("group_header")).sendKeys("test");
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test8");
+    driver.findElement(By.name("group_footer")).sendKeys("test");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
   }
